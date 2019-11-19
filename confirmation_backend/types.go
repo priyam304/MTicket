36cddefb12e3839a3e8cec22454d370dfe6ee3e9
jpathak6732 <54605061+jpathak6732@users.noreply.github.com>
2019-11-19 15:12:33 -0800

    Delete types.go

--- confirmation_backend/types.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type Person struct {
-	UserId string `json:"UserId,omitempty" bson:"UserId,omitempty"`
-	Name   string `json:"name,omitempty" bson:"name,omitempty"`
-	Email  string `json:"email,omitempty" bson:"email,omitempty"`
-	Mobile string `json:"mobile,omitempty" bson:"mobile,omitempty"`
-}

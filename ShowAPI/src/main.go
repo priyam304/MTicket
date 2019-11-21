620f7fdeae28dd43b0d70a6fc62fda3752037ac6
priyam304 <priyam.vaidya@sjsu.edu>
2019-11-21 15:56:47 -0800

    Updated routes in main.go

--- ShowAPI/src/main.go
+++ ShowAPI/src/main.go
@@ -14,20 +14,21 @@ import (
 
 func main() {
 
-    //mongoURI := "mongodb://cmpe281:cmpe281@10.0.1.244:27017"
-
     fmt.Println("Starting the application")
     ctx,_:= context.WithTimeout(context.Background(),10*time.Second)
 
-    //clientOptions:= options.Client().ApplyURI()
+    
     clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
     client,_=mongo.Connect(ctx,clientOptions)
     router:=mux.NewRouter()
     router.HandleFunc("/show", CreateShowEndpoint).Methods("POST")
-    router.HandleFunc("/show/{id}", GetShowEndpoint).Methods("GET")
-    router.HandleFunc("/shows", GetAllShowsEndpoint).Methods("GET")
     router.HandleFunc("/show/{id}", DeleteShowEndpoint).Methods("DELETE")
     router.HandleFunc("/show/{id}", UpdateShowEndpoint).Methods("PUT")
-    http.ListenAndServe(":12345",router)
-    //"mongodb://localhost:27017"
+    router.HandleFunc("/show/{id}", GetShowEndpoint).Methods("GET")
+    router.HandleFunc("/shows", GetAllShowsEndpoint).Methods("GET")
+    router.HandleFunc("/createbook/{id}", CreateBookingEndpoint).Methods("POST")
+    router.HandleFunc("/book/{id}", BookShowEndpoint).Methods("POST")
+    router.HandleFunc("/book/{id}", GetBookingEndpoint).Methods("GET")
+    router.HandleFunc("/bookings", GetAllBookingEndpoint).Methods("GET")
+    http.ListenAndServe(":3000",router)
 }
